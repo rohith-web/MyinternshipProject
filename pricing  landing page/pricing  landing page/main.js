c665b9913ee5b964f26596afa4ddaddfaca428e0
rohith-web <147065051+rohith-web@users.noreply.github.com>
2024-03-12 18:01:55 +0530

    main.js 1

--- pricing  landing page/pricing  landing page/main.js
+++ pricing  landing page/pricing  landing page/main.js
@@ -7,12 +7,12 @@ const ultraPrice = document.getElementById("ultra-price");
 toggler.addEventListener("change", ()=>{
     if(toggler.checked){
         starterPrice.innerHTML = `500Rs<span> / day </span>`;
-        proPrice.innerHTML = `2000Rs<span> / weak </span>`;
+        proPrice.innerHTML = `2000Rs<span> / week </span>`;
         ultraPrice.innerHTML = `9000Rs <span> / month </span>`;
 
     }else{
         starterPrice.innerHTML = `1000Rs <span> / day </span>`;
-        proPrice.innerHTML = '5000Rs<span> / weak </span>'; 
+        proPrice.innerHTML = '5000Rs<span> / week </span>'; 
         ultraPrice.innerHTML = `15000Rs<span> / month </span>`;
 
     }
