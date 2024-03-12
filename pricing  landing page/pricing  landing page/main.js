fdc4a0a6acfc6f6ab1cda1d7eac4e2cbae2e8711
rohith-web <147065051+rohith-web@users.noreply.github.com>
2024-03-12 17:45:21 +0530

    Add files via upload

---
+++ pricing  landing page/pricing  landing page/main.js
@@ -0,0 +1,19 @@
+const toggler = document.querySelector(".toggler");
+const starterPrice = document.getElementById("starter-price");
+const proPrice = document.getElementById("pro-price");
+const ultraPrice = document.getElementById("ultra-price");
+
+
+toggler.addEventListener("change", ()=>{
+    if(toggler.checked){
+        starterPrice.innerHTML = `500Rs<span> / day </span>`;
+        proPrice.innerHTML = `2000Rs<span> / weak </span>`;
+        ultraPrice.innerHTML = `9000Rs <span> / month </span>`;
+
+    }else{
+        starterPrice.innerHTML = `1000Rs <span> / day </span>`;
+        proPrice.innerHTML = '5000Rs<span> / weak </span>'; 
+        ultraPrice.innerHTML = `15000Rs<span> / month </span>`;
+
+    }
+})
